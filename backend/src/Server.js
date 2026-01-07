6c72e0dd72ef40f7dba173b415694371af7864f0
nagineninithinrao <nagineninithinrao399@gmail.com>
2026-01-07 22:19:53 +0530

    updated server path

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -24,6 +24,8 @@ mongoose
     console.error("MongoDB Error:", err);
     process.exit(1);
   });
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
